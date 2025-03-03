39f8315ae49d0c45e12e2c66962081bb0d1b8c11
Muhammad abiyyu <abiyyurahid20@gmail.com>
2025-03-03 23:47:34 +0700

    tambah fitur GUI

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,28 @@ namespace tpmodul03
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                label1.Text = "Halo " + textBox1.Text;
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
     }
 }
--- obj/Debug/net8.0-windows/tpmodul03.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/tpmodul03.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+414d27aa3e5f5114fd14c4e96437b072d2006df5")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
